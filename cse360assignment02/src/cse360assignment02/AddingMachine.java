fe3aa535d865670630300fd84bed5b0c56c1db65
shefali <shefali@10.110.1.170>
2020-10-05 18:50:52 -0700

    Second version of AddingMachine file with code implemented.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,63 +1,70 @@
 /**
  * @author Shefali Deosthali
- */		 
-package cse360assignment02; 
+ */	
+package cse360assignment02;
 
 public class AddingMachine 
 {
-	private int total; //total variable defined 
+	public static void main(String args[])
+	{
 	
+	}
+	private int total; //total variable defined
+	private String history; //String history defined
+			
 	/**
-	 * Method to initialize variables 
-	 */
+	* Method to initialize variables 
+	*/
 	public AddingMachine()
 	{
-		total = 0; //not needed - included for clarity
+		total = 0;
+		history = "0";
 	}
-	
+			
 	/**
-	 * Return the current total 
-	 * @return current total
-	 */
+	* Return the current total 
+	* @return current total
+	*/
 	public int getTotal()
 	{
-		return 0;
+		return total;
 	}
-	
+
 	/**
-	 * Add parameter to total variable 
-	 * @param int value, which will be added to total variable 
-	 */
+	* Add parameter to total variable 
+	* @param int value, which will be added to total variable 
+	*/
 	public void add(int value)
 	{
-		
+		total = total + value; 
+		history = history + " + " + String.valueOf(value); 
 	}
-	
+			
 	/**
-	 * Subtract parameter from total variable 
-	 * @param int value, which will be subtracted from total variable
-	 */
+	* Subtract parameter from total variable 
+	* @param int value, which will be subtracted from total variable
+	*/
 	public void subtract(int value)
 	{
-		
+		total = total - value; 
+		history = history + " - " + String.valueOf(value); 
 	}
-	
+			
 	/**
-	 * History of transactions starting from 0 value to the last input
-	 * @return history of transactions
-	 */
+	* History of transactions starting from 0 value to the last input
+	* @return history of transactions
+	*/
 	public String toString()
 	{
-		return ""; 
+		return history; 
 	}
-	
+			
 	/**
-	 * Clear method to clear "memory"
-	 */
+	* Clear method to clear "memory"
+	*/
 	public void clear()
 	{
-		
-	}		
+		total = 0;
+		history = "0";
+	}
 }
-
-
